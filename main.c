517bab3b4e164229990180c29edb93420c7ef57e
alemart <alemartf@gmail.com>
2017-02-19 22:28:54 -0300

    SurgeScript Compiler: first version of the lexer

--- main.c
+++ main.c
@@ -149,3 +149,46 @@ int main()
     /* done */
     return 0;
 }
+
+/* some code to test */
+#if 0
+// test
+// by alemart
+object "MyObject" {
+	requires "0.2.0"
+	length = 0;
+		$var = +2;
+		$2 = -1;
+		_3$ = ++3;
+		2ab2 = c;
+
+	state "main" {
+		a = 2;
+		a = a + 1;
+		a--;
+		++a;
+		b = -57;
+		pi = 3.14159;
+		msg += "Hello, world!"; // yeah!
+		"Hello", 'Hi', 'hmmm!! she says, "hey"', "I say 'hello'", hmm!
+		"Is this string \"right\"?" 'okay\'s'
+		"Multiline string"
+		hey there
+		this.test();
+
+        if(1 == 2)
+		    destroy();
+	}
+
+	/*@ hello
+	
+	*/2 ? 1 : 2;
+	fun test()
+	{
+		// hey!
+		app.exit();
+	}
+
+	@
+}
+#endif
