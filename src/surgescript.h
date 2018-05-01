806a9e879ab490940bf73d24828863fe73d00cfe
alemart <alemartf@gmail.com>
2018-04-30 22:56:57 -0300

    Included a surgescript.h header

---
+++ src/surgescript.h
@@ -0,0 +1,37 @@
+/*
+ * SurgeScript
+ * A scripting language for games
+ * Copyright 2016-2018 Alexandre Martins <alemartf(at)gmail(dot)com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * surgescript.h
+ * SurgeScript main header
+ */
+
+#ifndef _SURGESCRIPT_H
+#define _SURGESCRIPT_H
+
+#include "runtime/vm.h"
+#include "runtime/program.h"
+#include "runtime/object.h"
+#include "runtime/program_pool.h"
+#include "runtime/tag_system.h"
+#include "runtime/object_manager.h"
+#include "runtime/heap.h"
+#include "runtime/stack.h"
+#include "runtime/variable.h"
+#include "util/ssarray.h"
+#include "util/util.h"
+
+#endif
