bd7741953e995b33ac868f272a897b7f6511c6e2
alemart <alemartf@gmail.com>
2018-05-22 14:31:02 -0300

    Removed Application.session

--- src/surgescript/runtime/vm.c
+++ src/surgescript/runtime/vm.c
@@ -107,7 +107,6 @@ surgescript_vm_t* surgescript_vm_create()
     surgescript_sslib_register_console(vm);
     surgescript_sslib_register_transform2d(vm);
     surgescript_sslib_register_tagsystem(vm);
-    surgescript_sslib_register_session(vm);
     surgescript_sslib_register_plugin(vm);
     surgescript_sslib_register_arguments(vm);
     surgescript_sslib_register_application(vm);
